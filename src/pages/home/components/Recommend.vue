6ba1a3148c2b99d04f8c4ef5e3b4a33483115821
jirenguliukang <292582592@qq.com>
2018-11-22 00:20:47 +0800

    index-recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+            <img class="item-img" :src="item.imgURL" alt="">
+            <div class="item-info">
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+                <button class="item-button">查看 详情</button>
+            </div>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgURL: 'http://img1.qunarzz.com/sight/p0/1807/ad/ad574bfe6c9e1ceda3.img.png_200x200_ea7a0734.png',
+        title: '成都欢乐谷',
+        desc: '快乐成都首站，一个充满快乐的主题乐园'
+      }, {
+        id: '0002',
+        imgURL: 'http://img1.qunarzz.com/sight/p0/1807/ad/ad574bfe6c9e1ceda3.img.png_200x200_ea7a0734.png',
+        title: '成都欢乐谷',
+        desc: '快乐成都首站，一个充满快乐的主题乐园'
+      }, {
+        id: '0003',
+        imgURL: 'http://img1.qunarzz.com/sight/p0/1807/ad/ad574bfe6c9e1ceda3.img.png_200x200_ea7a0734.png',
+        title: '成都欢乐谷',
+        desc: '快乐成都首站，一个充满快乐的主题乐园'          
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+  .recommend-title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    display:flex
+    overflow: hidden
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+          line-height: .54rem
+          font-size: .32rem
+          ellipsis ()
+      .item-desc
+          list-height: .4rem
+          color: #ccc
+          ellipsis ()
+      .item-button
+          line-height: .44rem
+          margin-top: .16rem
+          background: #ff9300
+          padding: 0 .2rem
+          border-radius: .06rem
+          color: #fff
+</style>
