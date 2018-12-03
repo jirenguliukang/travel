1c3d2cc79f67e261cf810040d4196cb9085858b1
jirenguliukang <292582592@qq.com>
2018-12-04 00:20:19 +0800

    perfomance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-list :cities="cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities"
+    :letter="letter"
+    ></city-list>
+    <city-alphabet :cities="cities"
+    @change="handleLetterChange"
+    ></city-alphabet>
   </div>
 </template>
 <script>
@@ -20,7 +24,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -35,6 +40,9 @@ export default {
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
   },
   mounted () {
