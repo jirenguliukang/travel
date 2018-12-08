a5e1b515d7c4aa9769e2c13fa6dd03d792fe810c
jirenguliukang <292582592@qq.com>
2018-12-08 23:59:55 +0800

    city-vuex

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -5,7 +5,9 @@
   </div>
   <div class="search-content" ref="search" v-show="keyword">
     <ul>
-      <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+      <li class="search-item border-bottom" v-for="item of list" :key="item.id"
+      @click="handleCityClick(item.name)"
+      >
         {{item.name}}
       </li>
       <li class="search-item border-bottom" v-show="!list.length">
@@ -28,6 +30,12 @@ export default {
       keyword: '',
       list: [],
       timer: null
+    }
+  },
+  methods: {
+    handleCityClick (city) {
+      this.$store.dispatch('changeCity', city)
+      this.$router.push('/')
     }
   },
   watch: {
